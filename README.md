8c114fd932dd4415616e07a65f77d7a9b56c1eb8
Ben Wallberg <wallberg@umd.edu>
2022-05-13 09:07:45 -0400

    LIBWEB-5625. Create AdminTools searcher
    https://issues.umd.edu/browse/LIBWEB-5625

---
+++ README.md
@@ -0,0 +1,107 @@
+# fedora2-searcher
+
+Python 3 Flask application to search Fedora2 / AdminTools; provides a backend searcher to a Bento Box style search
+which expects a REST interface following the Quick Search model.
+
+## Requires
+
+* Python 3
+
+## Running the Webapp
+
+```bash
+# create a .env file (then manually update environment variables)
+$ cp .env-template .env
+```
+
+### Running locally
+
+```bash
+# install requirements
+$ pip install -r requirements.txt
+
+# run the app with Flask
+$ flask run
+```
+
+### Running in Docker
+
+```bash
+$ docker build -t fedora2-searcher .
+$ docker run -it --rm -p 5000:5000 --env-file=.env --read-only fedora2-searcher
+```
+
+### Endpoints
+
+This will start the webapp listening on the default port 5000 on localhost
+(127.0.0.1), and running in [Flask's debug mode].
+
+Root endpoint (just returns `{status: ok}` to all requests):
+<http://localhost:5000/>
+
+/ping endpoint (just returns `{status: ok}` to all requests):
+<http://localhost:5000/ping>
+
+/search endpoint: `http://localhost:5000/search?q={query}&page={page number?}&per_page={results per page?}`
+
+Example:
+
+```bash
+curl 'http://localhost:5000/search?q=kermit'
+{
+  "endpoint": "fedora2-search",
+  "module_link": "https://digital.lib.umd.edu/resultsnew?query=kermit",
+  "no_results_link": "https://digital.lib.umd.edu/resultsnew",
+  "page": "0",
+  "per_page": "3",
+  "query": "kermit",
+  "results": [
+    {
+      "description": "Young Jim Henson paints the finishing touches on Kermit the Frog, circa 1958.",
+      "extra": {
+        "collection": "University AlbUM",
+        "htmlSnippet": [
+          "Young Jim Henson puts finishing touches on <b>Kermit</b> the Frog, circa 1958"
+        ],
+        "thumbnail": "https://fedora.lib.umd.edu/fedora/get/umd:92324/umd-bdef:image/getThumbnail?maxHeight=110&maxWidth=110"
+      },
+      "item_format": "Image",
+      "link": "https://digital.lib.umd.edu/resultsnew/id/umd%3A92323",
+      "title": "Young Jim Henson puts finishing touches on Kermit the Frog, circa 1958"
+    },
+    {
+      "description": "Jim Henson with Kermit and Sweetums, two of his Muppets, at a University of Maryland football game, November 3, 1979.",
+      "extra": {
+        "collection": "University AlbUM",
+        "htmlSnippet": [
+          "Jim Henson with <b>Kermit</b> and Sweetums, University of Maryland, November 3, 1979"
+        ],
+        "thumbnail": "https://fedora.lib.umd.edu/fedora/get/umd:6065/umd-bdef:image/getThumbnail?maxHeight=110&maxWidth=110"
+      },
+      "item_format": "Image",
+      "link": "https://digital.lib.umd.edu/resultsnew/id/umd%3A6064",
+      "title": "Jim Henson with Kermit and Sweetums, University of Maryland, November 3, 1979"
+    },
+    {
+      "description": "Kermit and Miss Piggy at October 13, 1990 Homecoming Henson Tribute.",
+      "extra": {
+        "collection": "University AlbUM",
+        "htmlSnippet": [
+          "<b>Kermit</b> and Miss Piggy at 1990 Homecoming Henson Tribute"
+        ],
+        "thumbnail": "https://fedora.lib.umd.edu/fedora/get/umd:465957/umd-bdef:image/getThumbnail?maxHeight=110&maxWidth=110"
+      },
+      "item_format": "Image",
+      "link": "https://digital.lib.umd.edu/resultsnew/id/umd%3A465956",
+      "title": "Kermit and Miss Piggy at 1990 Homecoming Henson Tribute"
+    }
+  ],
+  "total": 42
+}
+```
+
+[Flask's debug mode]: https://flask.palletsprojects.com/en/2.0.x/quickstart/#debug-mode
+
+## License
+
+See the [LICENSE](LICENSE.txt) file for license rights and limitations.
---
+++ src/app.py
@@ -0,0 +1,187 @@
+import json
+import logging
+import os
+import sys
+import json
+import urllib.parse
+import requests
+import re
+
+import furl
+from flask import Flask, Response, request
+from dotenv import load_dotenv
+from waitress import serve
+from paste.translogger import TransLogger
+
+# Searcher for Fedora2 / AdminTools
+
+# Add any environment variables from .env
+load_dotenv('../.env')
+
+# Get environment variables
+env = {}
+for key in ('FEDORA2_SOLR_URL', 'FEDORA2_SOLR_FILTER_QUERY', 
+            'FEDORA2_LINK', 'FEDORA2_NO_RESULTS_LINK', 'FEDORA2_MODULE_LINK'):
+    env[key] = os.environ.get(key)
+    if env[key] is None:
+        raise RuntimeError(f'Must provide environment variable: {key}')
+
+solr_url = furl.furl(env['FEDORA2_SOLR_URL'])
+search_url = solr_url / 'select'
+search_fq = env['FEDORA2_SOLR_FILTER_QUERY']
+link = env['FEDORA2_LINK']
+no_results_link = env['FEDORA2_NO_RESULTS_LINK']
+module_link = env['FEDORA2_MODULE_LINK']
+
+debug = os.environ.get('FLASK_ENV') == 'development'
+
+logging.root.addHandler(logging.StreamHandler())
+
+loggerWaitress = logging.getLogger('waitress')
+logger = logging.getLogger('fedora2-searcher')
+
+if debug:
+    loggerWaitress.setLevel(logging.DEBUG)
+    logger.setLevel(logging.DEBUG)
+
+    # from http.client import HTTPConnection
+    # HTTPConnection.debuglevel = 1
+    # requests_log = logging.getLogger("requests.packages.urllib3")
+    # requests_log.setLevel(logging.DEBUG)
+    # requests_log.propagate = True
+else:
+    loggerWaitress.setLevel(logging.INFO)
+    logger.setLevel(logging.INFO)
+
+logger.info("Starting the fedora2-searcher Flask application")
+
+endpoint = 'fedora2-search'
+
+
+# Start the flask app, with compression enabled
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+
+@app.route('/')
+def root():
+    return {'status': 'ok'}
+
+
+@app.route('/ping')
+def ping():
+    return {'status': 'ok'}
+
+
+@app.route('/search')
+def search():
+
+    # Get the request parameters
+    args = request.args
+    if 'q' not in args or args['q'] == "":
+        return {
+            'endpoint': endpoint,
+            'error': {
+                'msg': 'q parameter is required',
+            },
+        }, 400
+    query = args['q']
+
+    per_page = 3
+    if 'per_page' in args and args['per_page'] != "":
+    	per_page = args['per_page']
+
+    page = 0
+    if 'page' in args and args['page'] != "" and args['page'] != "%":
+    	page = args['page']
+
+    start = int(page) * int(per_page)
+    rows = per_page
+
+    # Execute the search
+    params = {
+        'q': query,
+        'df': 'dmKeyword',
+        'rows': rows,  # number of results
+        'start': start,  # starting at this result (0 is the first result)
+        'fq': search_fq, # filter query
+        'wt': 'json', # get JSON format results
+        'sort': 'score desc',
+        'fl': 'displayTitle,itemType,dmDate,collectionTitle,pid,hensonDescription,thumbnail110',
+        'version': '2',
+        'hl': 'true',
+        'hl.fragsize': '500',
+        'hl.simple.pre': '<b>',
+        'hl.simple.post': '</b>',
+        'hl.fl': 'displayTitle',
+    }
+    try:
+        response = requests.get(search_url.url, params=params)
+    except Exception as err:
+        logger.error(f'Error submitting search url={search_url.url}, params={params}\n{err}')
+
+        return {
+            'endpoint': endpoint,
+            'error': {
+                'msg': f'Error submitting search',
+            },
+        }, 500
+
+    if response.status_code != 200:
+        logger.error(f'Received {response.status_code} when submitted {query=}')
+
+        return {
+            'endpoint': endpoint,
+            'error': {
+                'msg': f'Received {response.status_code} when submitted {query=}',
+            },
+        }, 500
+
+    logger.debug(f'Submitted url={search_url.url}, params={params}')
+    logger.debug(f'Received response {response.status_code}')
+    logger.debug(response.text)
+
+    data = json.loads(response.text)
+
+    # Gather the search results into our response
+    results = []
+    response = {
+        'endpoint': endpoint,
+        'query': query,
+        "per_page": str(per_page),
+        "page": str(page),
+        "total": int(data['response']['numFound']),
+        "module_link": module_link.replace('{query}',
+                                           urllib.parse.quote_plus(query)),
+        "no_results_link": no_results_link,
+        "results": results
+    }
+
+    if 'docs' in data['response']:
+        for item in data['response']['docs']:
+            id = item['pid']
+
+            results.append({
+                'title': item['displayTitle'],
+                'link': link.replace('{id}',
+                        urllib.parse.quote_plus(id)),
+                'description': item['hensonDescription'] if 'hensonDescription' in item else '',
+                'item_format': item['itemType'],
+                'extra': {
+                    'collection': item['collectionTitle'][0],
+                    'htmlSnippet': data['highlighting'][id]['displayTitle'],
+                    'thumbnail': item['thumbnail110'],
+                },
+            })
+
+    return response
+
+
+if __name__ == '__main__':
+    # This code is not reached when running "flask run". However the Docker
+    # container runs "python app.py" and host='0.0.0.0' is set to ensure
+    # that flask listens on port 5000 on all interfaces.
+
+    # Run waitress WSGI server
+    serve(TransLogger(app, setup_console_handler=True),
+          host='0.0.0.0', port=5000, threads=10)
